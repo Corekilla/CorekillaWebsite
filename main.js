836176e17196cfe13e0395af8e93c00686c369ce
Corekilla <42696700+Corekilla@users.noreply.github.com>
2023-03-20 16:28:34 +0900

    Add files via upload

---
+++ main.js
@@ -0,0 +1,29 @@
+
+window.onload = () =>{
+    // function sum(a,b){
+    //     return a + b;
+    // }
+    // let newSum =(a,b) => a+b
+
+    // let number = prompt("Number?");
+    
+    // alert(`Is your number ${number}?`);
+    
+    const docBody = document.body;
+    console.log(docBody);
+
+   
+    
+
+
+
+
+}
+function hover(img){
+    img.src = "/Odin/BackIn/img/CoreStampHighlightRef.png";
+}
+function hoverOut(img){
+    img.src = "/Odin/BackIn/img/CoreStamp.png";
+} 
+
+
